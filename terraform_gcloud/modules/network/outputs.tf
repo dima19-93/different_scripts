18c0002c8a751ab48fdbf894fce8f7e28877311c
Dima Simko <dima70593@ukr.net>
2025-10-11 17:09:18 +0300

    Ignore folder modules

--- terraform_gcloud/modules/network/outputs.tf
+++
@@ -1,6 +0,0 @@
-output "network_self_link" {
-  value = google_compute_network.vpc_network.self_link
-}
-output "subnet_self_link" {
-  value = google_compute_subnetwork.subnet.self_link 
-}
--- terraform_gcloud/modules/network/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "network_name" {}
-variable "subnet_name" {}
-variable "subnet_cidr" {}
-variable "region" {}
- 
